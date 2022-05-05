40f0a24735eb4b0bd9868806b4b9eeff41352739
Gareth <ghostwulf13@hotmail.com>
2022-05-04 20:01:43 -0600

    empire

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	doThis("Hello World!")
+	rad := add(69, 96)
+	dar := strconv.Itoa(rad)
+	fmt.Println("The sum of 69 and 96 is : " + dar)
+}
+
+func doThis(input string) {
+	fmt.Println(input)
+}
+
+func add(input1 int, input2 int) int {
+	var darth = input1 + input2
+	return darth
 }
